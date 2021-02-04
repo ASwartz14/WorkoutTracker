115f4b0825250a74e687825acb380091a24a7deb
ASwartz14 <asriley144@gmail.com>
2021-02-04 15:14:44 -0800

    creates models and server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewURLParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(require("./routes/html")(app));
+app.use(require("./routes/api")(app));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
